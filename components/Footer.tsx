0547a56ce2e2e1d36b1305bce5c0270df913710a
cvhlpont <pontchvl@gmail.com>
2025-11-30 01:14:41 +0100

    add footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="bg-dark-900 border-t border-dark-800 py-8">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="text-center text-dark-300">
+          <p>&copy; 2025 Coverforge. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  )
+}
