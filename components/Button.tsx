2f1eb698dff7e6f3e353dc7a822ea5d9dca663d7
cvhlpont <pontchvl@gmail.com>
2025-11-30 01:11:10 +0100

    add button component

---
+++ components/Button.tsx
@@ -0,0 +1,7 @@
+export default function Button({ children }: { children: React.ReactNode }) {
+  return (
+    <button className="px-10 py-4 bg-white hover:bg-gray-100 text-black rounded-xl font-bold text-lg transition-all transform hover:scale-105 shadow-xl border-2 border-white/20">
+      {children}
+    </button>
+  )
+}
