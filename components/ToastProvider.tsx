90b8b4a8821dcf24c3de83e47e78c3666ee71689
cvhlpont <pontchvl@gmail.com>
2025-11-30 21:29:21 +0100

    create toast provider

---
+++ components/ToastProvider.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { Toaster } from 'react-hot-toast'
+
+export default function ToastProvider() {
+  return (
+    <Toaster
+      position="top-center"
+      toastOptions={{
+        duration: 4000,
+        style: {
+          background: '#1e293b',
+          color: '#fff',
+          border: '1px solid #334155',
+        },
+        success: {
+          iconTheme: {
+            primary: '#4f46e5',
+            secondary: '#fff',
+          },
+        },
+        error: {
+          iconTheme: {
+            primary: '#ef4444',
+            secondary: '#fff',
+          },
+        },
+      }}
+    />
+  )
+}
