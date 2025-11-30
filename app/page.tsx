d4198c0efcb0e653f154a559a98195f69ecc11d0
cvhlpont <pontchvl@gmail.com>
2025-11-30 01:15:49 +0100

    add navbar and footer to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
+import Navbar from '@/components/Navbar'
 import Hero from '@/components/home/Hero'
 import HowItWorks from '@/components/home/HowItWorks'
 import Features from '@/components/home/Features'
 import CTA from '@/components/home/CTA'
+import Footer from '@/components/Footer'
 
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-dark-950">
+      <Navbar />
       <Hero />
       <HowItWorks />
       <Features />
       <CTA />
+      <Footer />
     </div>
   )
 }
